6970320dc8187ae383b5d228358d791945e5991f
Yozora0 <pablo.mourato@etudiant.univ-reims.fr>
2023-12-15 10:47:02 +0100

    init project and layout

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './global.css';
+import s from "./style.module.css";
+
+export function App() {
+    return (
+        <div className={s.main_container}>
+            <div className={s.header}>
+                <div className="row">
+                    <div className="col-4">
+                        <div>Logo here</div>
+                        <div>subtitle</div>
+                    </div>
+                    <div className="col-sm-12 col-md-4">
+                        <input style={{ width: "100%" }} type="text" />
+                    </div>
+                </div>
+            </div>
+            <div className={s.tv_show_details}>Tv show detail</div>
+            <div className={s.recommended_shows}>Recommended tv shows</div>
+        </div>
+    );
+}

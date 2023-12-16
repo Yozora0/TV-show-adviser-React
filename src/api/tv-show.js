5f77bdb6a1538427358c08428eff36e2d7f2956a
Yozora0 <pablo.mourato@etudiant.univ-reims.fr>
2023-12-16 16:46:44 +0100

    feat : creation and style component Recommended TV Show

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -5,7 +5,11 @@ export class TVShowAPI{
    static async fetchPopulars(){
       const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`)
         return response.data.results;
+    }
 
-
+    static async fetchRecommendations(tvShowId){
+        const response = await axios.get(
+            `${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`);
+        return response.data.results;
     }
 }

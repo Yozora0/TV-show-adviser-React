9a670a9a5eaa1016ce1aace9369a7495f062e3e7
Yozora0 <pablo.mourato@etudiant.univ-reims.fr>
2023-12-16 02:29:37 +0100

    feat : useEffect and display most popular tv show img

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './global.css';
 import s from "./style.module.css";
 import {TVShowAPI} from "./api/tv-show";
+import {useEffect, useState} from "react";
+import {BACKDROP_BASE_URL} from "./config";
 
 TVShowAPI.fetchPopulars()
 export function App() {
+    const [currentTVShow, setCurrentTVShow] = useState();
+
+    async function fetchPopulars() {
+        const populars = await TVShowAPI.fetchPopulars();
+        if (populars.length > 0) {
+            setCurrentTVShow(populars[0]);
+        }
+    }
+    useEffect(() => {
+        fetchPopulars();
+    }, []);
+
+    console.log("***", currentTVShow);
+
     return (
-        <div className={s.main_container}>
+        <div className={s.main_container} style={{ background : currentTVShow ?`linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover` : "black"}}>
             <div className={s.header}>
                 <div className="row">
                     <div className="col-4">
